c5a9f139daf564a5d417c93201caa1783b72ecb8
abhay734 <vermaabhay734@gmail.com>
2024-02-23 17:10:45 +0530

    Insert and Remove in LL

---
+++ 01_Linked_List/09_Insert.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1   
+        return True  
+  
+  
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(3)
+my_linked_list.append(31)
+my_linked_list.append(13)
+
+
+print('LL before insert():')
+my_linked_list.print_list()
+
+
+my_linked_list.insert(1,2)
+
+print('\nLL after insert(2) in middle:')
+my_linked_list.print_list()
+
+
+# my_linked_list.insert(0,0)
+
+# print('\nLL after insert(0) at beginning:')
+# my_linked_list.print_list()
+
+
+# my_linked_list.insert(4,4)
+
+# print('\nLL after insert(4) at end:')
+# my_linked_list.print_list()
---
+++ 01_Linked_List/10_Remove.py
@@ -0,0 +1,108 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        # If LL is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # If LL is not empty
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+    def pop(self):
+        # If LL is empty
+        if self.length == 0:
+            return None
+        # If two or more node are available in LL
+        else:
+            temp = self.head
+            pre = self.head
+            while temp.next is not None:
+                pre = temp
+                temp = temp.next 
+            self.tail = pre
+            # For popping last one
+            self.tail.next = None
+            self.length -= 1
+            # If there is only 1 node in LL
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp
+
+
+    def popfirst(self):
+        # If LL is empty
+        if self.length == 0:
+            return None
+        # If two or more node are available in LL
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+            # If there is only 1 node in LL
+            if self.length == 0:
+                self.tail = None
+            return temp
+
+
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.popfirst()
+        if index == self.length:
+            return self.pop()
+        prev = self.get(index-1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+        
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(3)
+my_linked_list.append(31)
+my_linked_list.append(13)
+
+
+print('LL before insert():')
+my_linked_list.print_list()
+
+my_linked_list.remove(2)
+
+print('\nLL after remove in middle:')
+my_linked_list.print_list()
