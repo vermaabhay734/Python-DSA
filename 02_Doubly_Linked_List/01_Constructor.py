176ee7cdf79660f39c0282d3cca3c11abdc795d5
abhay734 <vermaabhay734@gmail.com>
2024-02-26 17:58:22 +0530

    For creating new node in Doubly LL.

---
+++ 02_Doubly_Linked_List/01_Constructor.py
@@ -0,0 +1,27 @@
+# For creating new node in Doubly LL.
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.tail = None
+
+# For Creating Doubly LL
+class DoublyLinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    # Print Doubly LL
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+my_doubly_linked_list = DoublyLinkedList(4)
+
+print(my_doubly_linked_list.head.value)
