177ecbfd8cb4091cd3506da7974913877e155477
abhay734 <vermaabhay734@gmail.com>
2024-02-26 18:19:54 +0530

    Constructor in Doubly LL

--- 02_Doubly_Linked_List/01_Constructor.py
+++ 02_Doubly_Linked_List/01_Constructor.py
@@ -3,7 +3,8 @@ class Node():
     def __init__(self,value):
         self.value = value
         self.next = None
-        self.tail = None
+        self.prev = None
+
 
 # For Creating Doubly LL
 class DoublyLinkedList:
@@ -23,5 +24,8 @@ class DoublyLinkedList:
 
 
 my_doubly_linked_list = DoublyLinkedList(4)
+print('Head:', my_doubly_linked_list.head.value)
+print('Tail:', my_doubly_linked_list.tail.value)
+print('Length:', my_doubly_linked_list.length)
 
-print(my_doubly_linked_list.head.value)
+# print(my_doubly_linked_list.head.value)
