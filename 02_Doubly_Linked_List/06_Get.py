a4d9b8a14608df7381ac6b0791c06015ec265336
abhay734 <vermaabhay734@gmail.com>
2024-02-28 23:39:28 +0530

    Get in DLL

---
+++ 02_Doubly_Linked_List/06_Get.py
@@ -0,0 +1,66 @@
+# For creating new node in Doubly LL.
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+# For Creating Doubly LL
+class DoublyLinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    # Print Doubly LL
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+    def append(self, value):
+        new_node = Node(value)
+        # If Doubly LL is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # If Doubly LL is not empty
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length-1, index, -1):
+                temp = temp.prev
+        return temp.value
+    
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(12)
+my_doubly_linked_list.append(23)
+my_doubly_linked_list.append(33)
+
+
+print('Doubly Linked List:')
+my_doubly_linked_list.print_list()
+print("-----")
+print(my_doubly_linked_list.get(2))
+
+
