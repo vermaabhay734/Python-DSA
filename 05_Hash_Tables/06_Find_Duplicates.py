f0ec7628708ecfe6c44d074efdf96da86c69ff20
abhay734 <vermaabhay734@gmail.com>
2024-03-09 23:34:20 +0530

    Find Duplicates in List using HashTable

---
+++ 05_Hash_Tables/06_Find_Duplicates.py
@@ -0,0 +1,37 @@
+def find_duplicates(nums):
+    num_counts = {}
+    for num in nums:
+        num_counts[num] = num_counts.get(num, 0) + 1
+ 
+    duplicates = []
+    for num, count in num_counts.items():
+        if count > 1:
+            duplicates.append(num)
+
+    return duplicates
+        
+
+        
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
